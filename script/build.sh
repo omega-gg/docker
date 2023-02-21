55e979ab8a8c49d0980ec599b0747559f07dde35
bunjee <bunjee@omega.gg>
2023-02-21 18:14:24 +0100

    script: Add build.sh (8)

--- script/build.sh
+++ script/build.sh
@@ -113,7 +113,7 @@ $SUDO docker run \
     -d $IMGNAME || clean_up
 
 # wait for snapd to start
-TIMEOUT=100
+TIMEOUT=1000
 SLEEP=0.1
 echo -n "Waiting up to $(($TIMEOUT/10)) seconds for snapd startup "
 while [ "$($SUDO docker exec $CONTNAME sh -c 'systemctl status snapd.seeded >/dev/null 2>&1; echo $?')" != "0" ]; do
