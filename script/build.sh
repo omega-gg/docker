fbbcec8791e0104db71ae571dd838eea503fcba7
bunjee <bunjee@omega.gg>
2023-02-21 18:20:06 +0100

    script: Add build.sh (10)

--- script/build.sh
+++ script/build.sh
@@ -116,18 +116,20 @@ $SUDO docker run \
     -d $IMGNAME || clean_up
 
 # wait for snapd to start
-TIMEOUT=100
-SLEEP=0.1
-echo -n "Waiting up to $(($TIMEOUT/10)) seconds for snapd startup "
-while [ "$($SUDO docker exec $CONTNAME sh -c 'systemctl status snapd.seeded >/dev/null 2>&1; echo $?')" != "0" ]; do
-    echo -n "."
-    sleep $SLEEP || clean_up
-    if [ "$TIMEOUT" -le "0" ]; then
-        echo " Timed out!"
-        clean_up
-    fi
-    TIMEOUT=$(($TIMEOUT-1))
-done
+sleep 100
+
+#TIMEOUT=100
+#SLEEP=0.1
+#echo -n "Waiting up to $(($TIMEOUT/10)) seconds for snapd startup "
+#while [ "$($SUDO docker exec $CONTNAME sh -c 'systemctl status snapd.seeded >/dev/null 2>&1; echo $?')" != "0" ]; do
+#    echo -n "."
+#    sleep $SLEEP || clean_up
+#    if [ "$TIMEOUT" -le "0" ]; then
+#        echo " Timed out!"
+#        clean_up
+#    fi
+#    TIMEOUT=$(($TIMEOUT-1))
+#done
 echo " done"
 
 $SUDO docker exec $CONTNAME snap install core --edge || clean_up
